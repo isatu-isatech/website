fb27e82182a9b12ce24a585c149495a79306343e
ItsMikhaeli <josephmikhaeli.jalandoni@students.isatu.edu.ph>
2025-07-25 23:10:54 +0800

    feat: add new logo assets and update homepage layout with sections for stats, about, partners, and kwadra-tbi

--- src/app/(static)/page.tsx
+++ src/app/(static)/page.tsx
@@ -1,103 +1,164 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-center font-mono text-sm/6 sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="rounded bg-black/[.05] px-1 py-0.5 font-mono font-semibold dark:bg-white/[.06]">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <>
+      <div className="flex w-full flex-col items-center justify-center">
+        <StatsSection />
+        <AboutSection />
+        <PartnersSection />
+        <KwadraSection />
+      </div>
+    </>
+  );
+}
 
-        <div className="flex flex-col items-center gap-4 sm:flex-row">
-          <a
-            className="bg-foreground text-background flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent px-4 text-sm font-medium transition-colors hover:bg-[#383838] sm:h-12 sm:w-auto sm:px-5 sm:text-base dark:hover:bg-[#ccc]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="flex h-10 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm font-medium transition-colors hover:border-transparent hover:bg-[#f2f2f2] sm:h-12 sm:w-auto sm:px-5 sm:text-base md:w-[158px] dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+function StatsSection() {
+  const stats = [
+    { quantity: "5+", description: "Startups Established" },
+    { quantity: "25+", description: "Awards Earned" },
+    { quantity: "50+", description: "Events Participated" },
+  ];
+
+  return (
+    <>
+      <div className="flex w-full max-w-7xl flex-col px-6 py-16 lg:px-8 xl:px-16">
+        <div className="flex w-full flex-wrap items-center justify-center gap-y-6">
+          {stats.map((stat, index) => (
+            <div
+              key={index}
+              className="flex w-full flex-col items-center justify-center sm:w-1/2 md:w-1/3"
+            >
+              <h1>{stat.quantity}</h1>
+              <h3>{stat.description}</h3>
+            </div>
+          ))}
         </div>
-      </main>
-      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-[24px]">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </>
+  );
+}
+
+function AboutSection() {
+  return (
+    <>
+      <div className="flex w-full max-w-7xl flex-col px-6 py-16 lg:px-8 xl:px-16">
+        <div className="flex w-full flex-col items-center justify-center gap-6 md:flex-row-reverse">
+          <div className="flex w-full items-center justify-center md:w-1/2">
+            <Image
+              src="/assets/isatech-icon-dark.svg"
+              alt="ISATech Icon"
+              width={0}
+              height={0}
+              className="h-[150px] w-[100px] md:h-[270px] md:w-[185px]"
+            />
+          </div>
+          <div className="flex w-full flex-col items-center justify-center gap-4 md:w-1/2 md:gap-6">
+            <div className="flex w-full flex-col items-center justify-center gap-2 text-center md:items-end md:text-end">
+              <h2>What is ISATech Society?</h2>
+              <h5>
+                ISATech is a special interest organization operating under the
+                Intellectual Property Management Office (IPMO) and the Kwadra
+                Technology Business Incubator (Kwadra-TBI).
+              </h5>
+            </div>
+            <Button asChild>
+              <Link href="/">Learn More</Link>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function PartnersSection() {
+  const partners = [
+    {
+      src: "/assets/isatu-logo.png",
+      alt: "About Us",
+      width: 800,
+      height: 800,
+      className: "h-[75px] w-[75px] lg:h-[100px] lg:w-[100px]",
+    },
+    {
+      src: "/assets/kwadra-tbi-logo-dark.png",
+      alt: "About Us",
+      width: 1080,
+      height: 1080,
+      className: "h-[75px] w-[75px] lg:h-[100px] lg:w-[100px]",
+    },
+    {
+      src: "/assets/umwad-logo-dark.png",
+      alt: "About Us",
+      width: 1632,
+      height: 1381,
+      className: "h-[75px] w-[90px] lg:h-[100px] lg:w-[120px]",
+    },
+    {
+      src: "/assets/cci-sc-logo.png",
+      alt: "About Us",
+      width: 1080,
+      height: 1080,
+      className: "h-[75px] w-[75px] lg:h-[100px] lg:w-[100px]",
+    },
+  ];
+
+  return (
+    <>
+      <div className="flex w-full max-w-7xl flex-col px-6 py-16 lg:px-8 xl:px-16">
+        <div className="flex w-full flex-col items-center justify-center gap-6">
+          <h3 className="text-primary">Our Partners</h3>
+          <div className="flex w-full flex-wrap items-center justify-center gap-y-10 px-12 sm:justify-between md:justify-center md:gap-20">
+            {partners.map((partner, key) => (
+              <div className="flex w-1/2 justify-center sm:w-auto" key={key}>
+                <Image
+                  src={partner.src}
+                  alt={partner.alt}
+                  width={partner.width}
+                  height={partner.height}
+                  className={partner.className}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function KwadraSection() {
+  return (
+    <>
+      <div className="flex w-full max-w-7xl flex-col px-6 py-16 lg:px-8 xl:px-16">
+        <div className="flex w-full flex-col items-center justify-center gap-6 md:flex-row">
+          <div className="flex w-full items-center justify-center md:w-1/2">
+            <Image
+              src="/assets/kwadra-tbi-logo-dark.png"
+              alt="ISATech Icon"
+              width={1080}
+              height={1080}
+              className="h-[150px] w-[150px] md:h-[290px] md:w-[290px]"
+            />
+          </div>
+          <div className="flex w-full flex-col items-center justify-center gap-4 md:w-1/2 md:gap-6">
+            <div className="flex w-full flex-col items-center justify-center gap-2 text-center md:items-start md:text-start">
+              <h2>What is Kwadra-TBI?</h2>
+              <h5>
+                The Kwadra-TBI functions as a technology business incubator,
+                aiming to commercialize university research into startups and to
+                nurture deep technology startups.
+              </h5>
+            </div>
+            <Button asChild>
+              <Link href="/">Learn More</Link>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
