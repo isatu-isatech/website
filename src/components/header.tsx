6b4350978735f7a888a7e1955e7f93de42e15006
gjpgabayeron <gjpgabayeron@gmail.com>
2025-08-07 10:37:42 +0800

    feat: added right click navigation to view brandinng (TODO)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,6 +12,7 @@ import {
   SheetTrigger,
 } from "./ui/sheet";
 import { ISATechLogoMark } from "./assets/logos";
+import { redirect } from "next/navigation";
 
 /**
  * ################################################################################
@@ -36,7 +37,13 @@ export default function HeaderComponent() {
   return (
     <header className="border-b-grey-100 sticky top-0 z-90 flex items-center justify-center border-b bg-white/65 px-6 py-2 shadow-xs shadow-black/5 backdrop-blur-xs lg:px-8 xl:px-16">
       <div className="flex w-full max-w-6xl items-center justify-between">
-        <Link href="/">
+        <Link
+          href="/"
+          onContextMenu={(e) => {
+            e.preventDefault();
+            redirect("/about");
+          }}
+        >
           <ISATechLogoMark />
         </Link>
         <div className="flex items-center gap-2 md:gap-2 lg:gap-4">
@@ -75,14 +82,14 @@ export default function HeaderComponent() {
                   {/* This title is hidden but can be used for accessibility; Error occurs without this, don't ask why lol */}
                   <SheetTitle className="hidden">Navigation Menu</SheetTitle>
                 </SheetHeader>
-                <div className="flex flex-col items-start gap-1 px-2">
+                <div className="flex flex-col items-start gap-4 px-2">
                   {navLinks.map((link) => (
                     <Link
                       href={link.href}
                       className="w-full px-2 py-1"
                       key={link.label}
                     >
-                      <p className="text-caption">{link.label}</p>
+                      <h3>{link.label}</h3>
                     </Link>
                   ))}
                 </div>
