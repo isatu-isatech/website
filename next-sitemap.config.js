b34bce7c01a3aaae2ae2f4bbff6c646dfce91d1f
gjpgabayeron <gjpgabayeron@gmail.com>
2025-08-09 07:16:51 +0800

    feat: add sitemap configuration and robots.txt for improved SEO

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://isatech.club",
+  generateRobotsTxt: true, // (Optional)
+  // You can add more advanced options here if needed
+  // For example, excluding certain routes:
+  // exclude: ['/admin', '/private/*'],
+};
